0c1781cc55b475f077d9af05e6ced31121941796
ramenNoodles1998 <romanmeredith@gmail.com>
2022-10-28 00:05:15 -0600

    TODO

--- src/Components/NavBar/NavChip.js
+++ src/Components/NavBar/NavChip.js
@@ -1,5 +1,5 @@
 import './NavBar.css';
-
+//TODO: add background and on click reveal website.
 function NavChip(props) {
     return (
         <div className="bg-color-gray-900 cursor-pointer text-white font-mono border-2 border-gray-400 m-3 p-3 rounded-full">
